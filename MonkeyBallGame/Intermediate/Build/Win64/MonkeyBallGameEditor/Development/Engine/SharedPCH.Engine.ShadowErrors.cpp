8b4ed56b99d490683ab629e33c4d7f926030e54a
Thunder19 <braydenhamous@gmail.com>
2020-10-28 10:55:46 -0500

    1

---
+++ MonkeyBallGame/Intermediate/Build/Win64/MonkeyBallGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/Brayden/Documents/MonkeyBall/Monekey_Ball/MonkeyBallGame/Intermediate/Build/Win64/MonkeyBallGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ MonkeyBallGame/Intermediate/Build/Win64/UE4Editor/Inc/MonkeyBallGame/MonkeyBallGameGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef MONKEYBALLGAME_MonkeyBallGameGameModeBase_generated_h
+#error "MonkeyBallGameGameModeBase.generated.h already included, missing '#pragma once' in MonkeyBallGameGameModeBase.h"
+#endif
+#define MONKEYBALLGAME_MonkeyBallGameGameModeBase_generated_h
+
+#define MonkeyBallGame_Source_MonkeyBallGame_MonkeyBallGameGameModeBase_h_15_SPARSE_DATA
+#define MonkeyBallGame_Source_MonkeyBallGame_MonkeyBallGameGameModeBase_h_15_RPC_WRAPPERS
+#define MonkeyBallGame_Source_MonkeyBallGame_MonkeyBallGameGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define MonkeyBallGame_Source_MonkeyBallGame_MonkeyBallGameGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAMonkeyBallGameGameModeBase(); \
+	friend struct Z_Construct_UClass_AMonkeyBallGameGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AMonkeyBallGameGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/MonkeyBallGame"), NO_API) \
+	DECLARE_SERIALIZER(AMonkeyBallGameGameModeBase)
+
+
+#define MonkeyBallGame_Source_MonkeyBallGame_MonkeyBallGameGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAMonkeyBallGameGameModeBase(); \
+	friend struct Z_Construct_UClass_AMonkeyBallGameGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AMonkeyBallGameGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/MonkeyBallGame"), NO_API) \
+	DECLARE_SERIALIZER(AMonkeyBallGameGameModeBase)
+
+
+#define MonkeyBallGame_Source_MonkeyBallGame_MonkeyBallGameGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AMonkeyBallGameGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMonkeyBallGameGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMonkeyBallGameGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMonkeyBallGameGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMonkeyBallGameGameModeBase(AMonkeyBallGameGameModeBase&&); \
+	NO_API AMonkeyBallGameGameModeBase(const AMonkeyBallGameGameModeBase&); \
+public:
+
+
+#define MonkeyBallGame_Source_MonkeyBallGame_MonkeyBallGameGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AMonkeyBallGameGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMonkeyBallGameGameModeBase(AMonkeyBallGameGameModeBase&&); \
+	NO_API AMonkeyBallGameGameModeBase(const AMonkeyBallGameGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMonkeyBallGameGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMonkeyBallGameGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMonkeyBallGameGameModeBase)
+
+
+#define MonkeyBallGame_Source_MonkeyBallGame_MonkeyBallGameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define MonkeyBallGame_Source_MonkeyBallGame_MonkeyBallGameGameModeBase_h_12_PROLOG
+#define MonkeyBallGame_Source_MonkeyBallGame_MonkeyBallGameGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MonkeyBallGame_Source_MonkeyBallGame_MonkeyBallGameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	MonkeyBallGame_Source_MonkeyBallGame_MonkeyBallGameGameModeBase_h_15_SPARSE_DATA \
+	MonkeyBallGame_Source_MonkeyBallGame_MonkeyBallGameGameModeBase_h_15_RPC_WRAPPERS \
+	MonkeyBallGame_Source_MonkeyBallGame_MonkeyBallGameGameModeBase_h_15_INCLASS \
+	MonkeyBallGame_Source_MonkeyBallGame_MonkeyBallGameGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define MonkeyBallGame_Source_MonkeyBallGame_MonkeyBallGameGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MonkeyBallGame_Source_MonkeyBallGame_MonkeyBallGameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	MonkeyBallGame_Source_MonkeyBallGame_MonkeyBallGameGameModeBase_h_15_SPARSE_DATA \
+	MonkeyBallGame_Source_MonkeyBallGame_MonkeyBallGameGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	MonkeyBallGame_Source_MonkeyBallGame_MonkeyBallGameGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	MonkeyBallGame_Source_MonkeyBallGame_MonkeyBallGameGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> MONKEYBALLGAME_API UClass* StaticClass<class AMonkeyBallGameGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID MonkeyBallGame_Source_MonkeyBallGame_MonkeyBallGameGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
